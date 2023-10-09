485573ee44ed2f46e683796d9c02a6343ef34b44
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-10-09 00:29:09 -0300

    Update RecuperarPass.js

--- src/pages/RecuperarPass.js
+++ src/pages/RecuperarPass.js
@@ -34,7 +34,7 @@ export default function RecuperarPass() {
         localStorage.removeItem("recuperarToken");
       }, 2 * 60 * 1000);
 
-      fetch("http://localhost:4000/recuperar-password", {
+      fetch("https://back-comunidad.vercel.app/recuperar-password", {
         method: "POST",
         body: JSON.stringify({ email, token: newToken }),
         headers: {
